51763cf96482dc43424e9b0ad667494c572e0679
Scottl4784 <Scottl4784@gmail.com>
2018-07-24 14:20:54 -0400

    added destroy route to users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,6 +14,11 @@ class Api::UsersController < ApplicationController
         render json: @user
     end
 
+    def destroy
+        @user = User.destroy(params[:id])
+        render json: @user
+    end
+
     private
 
     def user_params
