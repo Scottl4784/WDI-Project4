8191decd8bfefcb245503f95b135a1695522d38d
Scottl4784 <Scottl4784@gmail.com>
2018-07-24 14:16:23 -0400

    fixed npm i mistake

---
+++ client/src/components/BeerSearch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BeerSearch extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default BeerSearch;
