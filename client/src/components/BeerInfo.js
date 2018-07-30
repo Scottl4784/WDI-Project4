46d97b8005d9671f582987d9355b9fe6276d94ab
Scottl4784 <Scottl4784@gmail.com>
2018-07-30 13:27:04 -0400

    added route to individual beer info

--- client/src/components/BeerInfo.js
+++ client/src/components/BeerInfo.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { Card, Image, Item } from 'semantic-ui-react'
 
 class BeerInfo extends Component {
+    state = {
+        beer: []
+    }
+
+    getBeer() {
+        const beerId = this.props.match.params.beerId
+        axios.get(`https://api.punkapi.com/v2/beers/${beerId}`).then((res) => {
+            this.setState({ beer: res.data[0] })
+        })
+    }
+    componentDidMount() {
+        this.getBeer()
+    }
     render() {
         return (
-            <div>
-                
-            </div>
+            <Item>
+            <Item.Image size='mini' src={this.state.beer.image_url} />
+      
+            <Item.Content>
+              <Item.Header as='a'>{this.state.beer.name}</Item.Header>
+              <Item.Description>
+                <p>{this.state.beer.description}</p>
+              </Item.Description>
+            </Item.Content>
+          </Item>
         );
     }
 }
